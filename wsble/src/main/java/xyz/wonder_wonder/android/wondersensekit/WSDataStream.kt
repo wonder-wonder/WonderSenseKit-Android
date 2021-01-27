d1b698fe0a72b00085a94b428b8f022db71a5d40
marsh31 <marsh22h53@gmail.com>
2021-01-27 19:57:15 +0900

    fix: add

--- wsble/src/main/java/xyz/wonder_wonder/android/wondersensekit/WSDataStream.kt
+++ wsble/src/main/java/xyz/wonder_wonder/android/wondersensekit/WSDataStream.kt
@@ -1,31 +1,31 @@
 package xyz.wonder_wonder.android.wondersensekit
 
-import xyz.wonder_wonder.android.wondersensekit.WSFilter
-import xyz.wonder_wonder.android.wondersensekit.WSFilterResult
+//import xyz.wonder_wonder.android.wondersensekit.WSFilter
+//import xyz.wonder_wonder.android.wondersensekit.WSFilterResult
 
 class WSDataStream {
     private var rawData_cbFunc: ((data: WSBLEData) -> Unit)? = null
-    private var filteredData_cbFunc: ((result: WSFilterResult) -> Unit)? = null
-    private var filter: WSFilter? = null
+//    private var filteredData_cbFunc: ((result: WSFilterResult) -> Unit)? = null
+//    private var filter: WSFilter? = null
 
     fun setRawDataCB(cbFunc: (data: WSBLEData) -> Unit) {
         rawData_cbFunc = cbFunc
     }
 
-    fun setFilteredDataCB(filter: WSFilter, cbFunc: (WSFilterResult)-> Unit) {
-        this.filter = filter
-        filteredData_cbFunc = cbFunc
-    }
+//    fun setFilteredDataCB(filter: WSFilter, cbFunc: (WSFilterResult)-> Unit) {
+//        this.filter = filter
+//        filteredData_cbFunc = cbFunc
+//    }
 
     fun receiveData(data: Array<WSBLEData>) {
         for (item in data) {
             rawData_cbFunc?.invoke(item)
         }
 
-        for (item in data) {
-            filter?.filter(item).let {
-                it?.let { it1 -> filteredData_cbFunc?.invoke(it1) }
-            }
-        }
+//        for (item in data) {
+//            filter?.filter(item).let {
+//                it?.let { it1 -> filteredData_cbFunc?.invoke(it1) }
+//            }
+//        }
     }
 }
